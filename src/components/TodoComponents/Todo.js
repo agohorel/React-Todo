4a1bfa1217f04903baa651693746d292beb962f2
agohorel <gohorelalex@gmail.com>
2020-01-13 16:00:17 -0500

    add strikethrough on completed todos

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export const Todo = ({ todo, toggleComplete }) => {
+  const strikeThrough = todo.completed ? "line-through" : "none";
   return (
     <div onClick={() => toggleComplete(todo.id)}>
-      <h2>{todo.task}</h2>
+      <h2 style={{ textDecoration: strikeThrough }}>{todo.task}</h2>
     </div>
   );
 };
